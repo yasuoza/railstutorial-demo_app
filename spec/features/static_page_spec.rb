4b4ed038b7f3db31a62ff4cab4250f45b8383220
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2014-01-23 08:15:48 +0900

    Finish 5.1-5.3

--- spec/features/static_page_spec.rb
+++ spec/features/static_page_spec.rb
@@ -2,42 +2,61 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+  subject { page }
 
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h1', :text => 'Sample App') }
+
+    it "should have the full_title('')" do
+      expect(page.title).to eql full_title('')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
       expect(page.title).to eql "Ruby on Rails Tutorial Sample App"
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+    it { should have_selector('h1', :text => 'Help') }
+
+    it "should have the full_title('Help')" do
+      expect(page.title).to eql full_title('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
       expect(page.title).to eql "Ruby on Rails Tutorial Sample App | Help"
     end
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+    it { should have_selector('h1', :text => 'About Us') }
+
+    it "should have the full_title('About Us')" do
+      expect(page.title).to eql full_title('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
       expect(page.title).to eql "Ruby on Rails Tutorial Sample App | About Us"
+    end
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1', text: 'Contact') }
+
+    it "should have the full_title('Contact')" do
+      expect(page.title).to eql full_title('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      expect(page.title).to eql "Ruby on Rails Tutorial Sample App | Contact"
     end
   end
 end

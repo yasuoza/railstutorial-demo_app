efe38f7aad619e9c9c4a95e18def19292d39dbfe
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2014-01-25 00:15:27 +0900

    Finish 11.1 The Relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,18 @@
+# == Schema Information
+#
+# Table name: relationships
+#
+#  id          :integer          not null, primary key
+#  follower_id :integer
+#  followed_id :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Relationship < ActiveRecord::Base
-  belongs_to :follower
-  belongs_to :followered
+  belongs_to :follower, class_name: :User
+  belongs_to :followed, class_name: :User
+
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,10 +15,16 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: :follower_id, dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: :followed_id,
+                                   class_name: :Relationship,
+                                   dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   before_save { |user| user.email = email.downcase }
   before_create :create_remember_token
-
-  has_many :microposts, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -38,6 +44,18 @@ class User < ActiveRecord::Base
   def feed
     # This is preliminary. See "Following users" for the full implementation.
     Micropost.where("user_id = ?", id)
+  end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
   end
 
   private

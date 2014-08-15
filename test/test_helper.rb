e00ae480c166ddc21fd2958066d7912a885dcd28
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2014-08-15 18:25:24 +0900

    Cleanup with DatabaseRewinder

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,6 +21,12 @@ class ActiveSupport::TestCase
   # -- they do not yet inherit this setting
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  setup do
+    DatabaseRewinder.clean_all
+  end
+
+  teardown do
+    DatabaseRewinder.clean
+  end
 end
 
